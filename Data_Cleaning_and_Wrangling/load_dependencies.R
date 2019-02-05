555fc078c85edc09421e78dbf96d79bd2c2e7429
Ben H-M <bhh1@rice.edu>
2019-02-04 19:51:40 -0600

    Update drake module

--- Data_Cleaning_and_Wrangling/load_dependencies.R
+++ Data_Cleaning_and_Wrangling/load_dependencies.R
@@ -1,6 +1,6 @@
-list.of.packages <- c("dplyr")
+list.of.packages <- c("tidyverse")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-library(dplyr)
+library(tidyverse)
